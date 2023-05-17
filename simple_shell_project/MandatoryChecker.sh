f6ff1733a76a5a1d703e4be0d95b20b90b17848a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-05-17 16:35:19 +0200

    Update MandatoryChecker.sh

--- simple_shell_project/MandatoryChecker.sh
+++ simple_shell_project/MandatoryChecker.sh
@@ -78,24 +78,6 @@ echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
 run_check ""echo" "
 
-#####################################
-# To add your own tests
-#####################################
-# 1- store the your shell ouput (out and err) in command1
-command1=$(echo "`#Your test`" | ./hsh 2>&1)
-
-# 2- store sh ouput (out and err) in command2
-command2=$(echo "`#Your test`" | sh 2>&1)
-
-# 3- sote command1 in 1a2b3c4d5e6f7g8h9i file
-echo "$command1" > 1a2b3c4d5e6f7g8h9i
-
-# 4- sote command2 in 1a2b3c4d5e6f7g8h9b file
-echo "$command2" > 1a2b3c4d5e6f7g8h9b
-
-# 5- Call run_check and pass comment to it
-run_check " `#Test comment` "
-
 #>>>> betty testing  <<<<<<
 
 #remove the hashs to enable betty testing
