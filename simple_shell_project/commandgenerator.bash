95815078d2f6262f4f336ff906315a2c9cb6e230
DestinedCodes <yiradesat@gmail.com>
2023-08-15 15:05:09 +0100

    fix broken general command generator

--- simple_shell_project/commandgenerator.bash
+++ simple_shell_project/commandgenerator.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Check if the current directory contains a ".bash" file
-if [ -n "$(find . -maxdepth 1 -type f -name '*.bash' ! -name 'checker.bash' ! -name 'commandgenerator.bash' ! -name 'runchecker.bash')" ]; then
-    echo "#####   $(basename "$PWD")   #####"
+# Get the list of directories where the script is executed and sort them
+directories=$(find . -maxdepth 1 -type d -name '[0-9]*' | sort -V)
 
-    # Iterate over the script files in the current directory
-    for script in ./*.bash; do
-        # Exclude specified files from the command list
-        if [ "$(basename "$script")" != "checker.bash" ] && [ "$(basename "$script")" != "commandgenerator.bash" ] && [ "$(basename "$script")" != "runchecker.bash" ]; then
-            echo "../checker.bash ../hsh ./$script"
-        fi
-    done
-fi > command_list.txt
+# Iterate over the directories
+for dir in $directories; do
+    # Check if the directory contains a ".bash" file
+    if [ -n "$(find "$dir" -maxdepth 1 -type f \( -name '*.bash' ! -name 'checker.bash' \))" ]; then
+        # Write the folder name in all caps
+        folder_name=$(basename "$dir" | tr '[:lower:]' '[:upper:]')
+        echo "#####   $folder_name   #####"
+
+        # Iterate over the script files in each directory
+        for script in "$dir"/*.bash; do
+            # Exclude "checker.bash" from the command list
+            if [ "$(basename "$script")" != "commandgenerator.bash" ] && [ "$(basename "$script")" != "runchecker.bash" ]; then
+                echo "./checker.bash ./hsh ./$(basename "$dir")/$(basename "$script")"
+            fi
+        done
+    fi
+done > command_list.txt
 
 
